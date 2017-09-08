a9fbd23e369d57e1380375bc42f4f18892560448
Thilo Maier <git@maier.asia>
2017-09-07 23:16:58 -0400

    Add Footer

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Box, Container, Flex, Lead, Link, Text } from 'rebass';
+import PropTypes from 'prop-types';
+import styled, { ThemeProvider, withTheme } from 'styled-components';
+import { color, fontSize, space } from 'styled-system';
+import { merge } from '../theme';
+
+const Footer = props => {
+  const InternalLink = styled(props.internalLinkComponent)`
+    ${color} ${fontSize} ${space} text-decoration: none;
+  `;
+  const ExternalLink = styled(Link)`
+    ${color} ${fontSize} ${space} text-decoration: none;
+  `;
+
+  return (
+    <ThemeProvider theme={merge}>
+      <Box is="footer" bg="primaryDarker" color="bgAlt">
+        <Container>
+          <Flex direction="column" justify="center" align="center">
+            <Lead py={[2, 3]}>
+              <InternalLink to="/" color="bgAlt">
+                {props.title}
+              </InternalLink>
+            </Lead>
+            <Flex
+              direction={['column', 'row']}
+              justify="space-around"
+              align="center"
+              wrap
+              width={1}
+            >
+              {props.internalLinks.map(link => (
+                <Box key={link.to} p={[1, 2]}>
+                  <InternalLink to={link.to} color="bgAlt" exact>
+                    {link.text}
+                  </InternalLink>
+                </Box>
+              ))}
+              {props.externalLinks.map(link => (
+                <Box key={link.to} p={[1, 2]}>
+                  <ExternalLink href={link.to} color="bgAlt">
+                    {link.text}
+                  </ExternalLink>
+                </Box>
+              ))}
+            </Flex>
+            <Text py={[2, 3]}>Made by @mdotasia</Text>
+          </Flex>
+        </Container>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+Footer.propTypes = {
+  /**
+   * Title for footer.
+   */
+  title: PropTypes.string.isRequired,
+  /**
+   * Constructor for internal link component:
+   * [React Router <NavLink>](https://reacttraining.com/react-router/web/api/NavLink) or any link
+   * component based on it, e.g.
+   * [Gatsby <Link>](https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-link).
+   */
+  internalLinkComponent: PropTypes.func.isRequired,
+  /**
+   * Array of internal links.
+   */
+  internalLinks: PropTypes.arrayOf(
+    PropTypes.shape({ to: PropTypes.string, text: PropTypes.string })
+  ),
+  /**
+   * Array of external links.
+   */
+  externalLinks: PropTypes.arrayOf(
+    PropTypes.shape({ to: PropTypes.string, text: PropTypes.string })
+  ),
+};
+
+Footer.defaultProps = {
+  internalLinks: [],
+  externalLinks: [],
+};
+
+export default withTheme(Footer);
---
+++ src/Footer/Footer.story.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Provider } from 'rebass';
+import { MemoryRouter } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import { colors } from '../theme';
+import Footer from './Footer';
+
+const internalLinks = [
+  { to: '/archive/', text: 'Archive' },
+  { to: '/about/', text: 'About' },
+];
+const externalLinks = [
+  { to: 'http://twitter.com/mdotasia', text: 'Twitter' },
+  { to: 'https://github.com/mdotasia/', text: 'GitHub' },
+];
+const title = 'Coding for the Web';
+
+// TODO use standard colors and override colors in one test
+
+// TODO One to make elements wrap
+
+storiesOf('components/Footer', module)
+  .add('internal and external links', () => (
+    <MemoryRouter>
+      <Provider theme={{ colors }}>
+        <Footer
+          title={title}
+          internalLinkComponent={NavLink}
+          internalLinks={internalLinks}
+          externalLinks={externalLinks}
+        />
+      </Provider>
+    </MemoryRouter>
+  ))
+  .add('internal links only', () => (
+    <MemoryRouter>
+      <Provider theme={{ colors }}>
+        <Footer
+          title={title}
+          internalLinkComponent={NavLink}
+          internalLinks={internalLinks}
+        />
+      </Provider>
+    </MemoryRouter>
+  ))
+  .add('external links only', () => (
+    <MemoryRouter>
+      <Provider theme={{ colors }}>
+        <Footer
+          title={title}
+          internalLinkComponent={NavLink}
+          externalLinks={externalLinks}
+        />
+      </Provider>
+    </MemoryRouter>
+  ))
+  .add('no links', () => (
+    <MemoryRouter>
+      <Provider theme={{ colors }}>
+        <Footer title={title} internalLinkComponent={NavLink} />
+      </Provider>
+    </MemoryRouter>
+  ))
+  .add('links wrap', () => (
+    <MemoryRouter>
+      <Provider theme={{ colors }}>
+        <Footer
+          title={title}
+          internalLinkComponent={NavLink}
+          internalLinks={[
+            { to: '/1/', text: 'One' },
+            { to: '/2/', text: 'Two' },
+            { to: '/3/', text: 'Three' },
+            { to: '/4/', text: 'Four' },
+            { to: '/5/', text: 'Five' },
+            { to: '/6/', text: 'Six' },
+            { to: '/7/', text: 'Seven' },
+            { to: '/8/', text: 'Eight' },
+            { to: '/9/', text: 'Nine' },
+            { to: '/10/', text: 'Ten' },
+            { to: '/11/', text: 'Eleven' },
+            { to: '/12/', text: 'Twelve' },
+            { to: '/13/', text: 'Thirteen' },
+            { to: '/14/', text: 'Fourteen' },
+            { to: '/15/', text: 'Fifteen' },
+            { to: '/16/', text: 'Sixteen' },
+            { to: '/17/', text: 'Seventeen' },
+            { to: '/18/', text: 'Eighteen' },
+            { to: '/19/', text: 'Nineteen' },
+            { to: '/20/', text: 'Twenty' },
+          ]}
+        />
+      </Provider>
+    </MemoryRouter>
+  ));

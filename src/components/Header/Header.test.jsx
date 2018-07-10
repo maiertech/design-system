a363235b6f912c034df35faa6e17b1d28b0d7f0b
Thilo Maier <git@maier.asia>
2018-07-10 10:14:32 -0400

    Use jest-puppeteer for visual regression tests

--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -1,26 +1,14 @@
-import puppeteer from "puppeteer";
 import * as deviceDescriptors from "puppeteer/DeviceDescriptors";
 import path from "path";
 import devices from "../devices";
 
 const url =
   "http://localhost:9009/?selectedKind=Header&selectedStory=default%20anchor&full=1";
-
 const platform = process.platform;
 
 describe("Header", () => {
-  let browser;
-
-  beforeAll(async () => {
-    // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md
-    browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    });
-  });
-
   devices.forEach(device => {
     test(device, async () => {
-      const page = await browser.newPage();
       await page.emulate(deviceDescriptors[device]);
       await page.goto(url);
       const image = await page.screenshot();
@@ -33,9 +21,5 @@ describe("Header", () => {
         customSnapshotIdentifier: device.toLocaleLowerCase().replace(/ /g, "-")
       });
     });
-  });
-
-  afterAll(async () => {
-    await browser.close();
   });
 });
--- src/components/PostList/PostList.test.js
+++ src/components/PostList/PostList.test.js
@@ -1,26 +1,14 @@
-import puppeteer from "puppeteer";
 import * as deviceDescriptors from "puppeteer/DeviceDescriptors";
 import path from "path";
 import devices from "../devices";
 
 const url =
   "http://localhost:9009/?selectedKind=PostList&selectedStory=default%20anchor&full=1";
-
 const platform = process.platform;
 
 describe("Footer", () => {
-  let browser;
-
-  beforeAll(async () => {
-    // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md
-    browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    });
-  });
-
   devices.forEach(device => {
     test(device, async () => {
-      const page = await browser.newPage();
       await page.emulate(deviceDescriptors[device]);
       await page.goto(url);
       // Full page screenshot is broken.
@@ -34,9 +22,5 @@ describe("Footer", () => {
         customSnapshotIdentifier: device.toLocaleLowerCase().replace(/ /g, "-")
       });
     });
-  });
-
-  afterAll(async () => {
-    await browser.close();
   });
 });

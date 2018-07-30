a5354bdbd97235ccb05991cc483f51484c313200
Thilo Maier <git@maier.asia>
2018-07-30 16:21:56 -0400

    Fix exports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-export { default as A } from "./A";
-export { default as Container } from "./Container";
-export { default as Footer } from "./Footer";
-export { default as Header } from "./Header";
-export { default as P } from "./P";
-export { default as PostHeader } from "./PostHeader";
-export { default as PostList } from "./PostList";
-export { default as Provider } from "./Provider";
+import A from "./components/A";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import P from "./components/P";
+import PostHeader from "./components/PostHeader";
+import PostList from "./components/PostList";
+import Provider from "./components/Provider";
+
+export { A, Container, Footer, Header, P, PostHeader, PostList, Provider };

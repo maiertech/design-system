660d79afba56315dc9616e8364ef8432125e23d2
Thilo Maier <git@maier.asia>
2018-07-30 13:27:37 -0400

    Add PostHeader

---
+++ src/components/PostHeader/PostHeader.stories.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import PostHeader from "./PostHeader";
+import Box from "../Box";
+
+storiesOf("PostHeader", module)
+  .addDecorator(story => <Box m={1}>{story()}</Box>)
+  .add("short title", () => (
+    <PostHeader
+      author="Thilo Maier"
+      date="Aug 1, 2017"
+      title="Cras ut malesuada"
+    />
+  ))
+  .add("long title", () => (
+    <PostHeader
+      author="Thilo Maier"
+      date="Aug 1, 2017"
+      title="Sed rhoncus mi eu quam varius rutrum morbi eget condimentum velit nam sit amet est non tortor ullamcorper fringilla."
+    />
+  ));
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ export { default as Container } from "./Container";
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as P } from "./P";
+export { default as PostHeader } from "./PostHeader";
 export { default as PostList } from "./PostList";
 export { default as Provider } from "./Provider";

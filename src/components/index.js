14128f52d8c19c97cc17527290649bbfeeabcf98
Thilo Maier <git@maier.asia>
2018-09-11 19:31:11 +0200

    Remove Container

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,6 @@
-import Container from "./Container";
 import Footer from "./Footer";
 import Header from "./Header";
 import PostHeader from "./PostHeader";
 import PostList from "./PostList";
-import Provider from "./Provider";
 
-export { Container, Footer, Header, PostHeader, PostList, Provider };
+export { Footer, Header, PostHeader, PostList };

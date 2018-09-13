382b7dc173f07577b868e36414da5ce4503c3209
Thilo Maier <git@maier.asia>
2018-09-12 23:11:20 -0400

    Create Page component

---
+++ src/pages/Page/Page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Box } from "rebass";
+import Helmet from "react-helmet";
+
+const Page = ({ title, description, children }) => (
+  <Box mb={[2, 3]}>
+    <Helmet>
+      <meta property="og:description" content={description} v />
+      <title>{title}</title>
+    </Helmet>
+    {children}
+  </Box>
+);
+
+Page.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
+};
+
+export default Page;

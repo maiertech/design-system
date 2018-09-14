2052763bb73304a25965b8d7598727801d9bf606
Thilo Maier <git@maier.asia>
2018-09-14 10:10:57 -0400

    Increase screenshot error threshold to 0.5%

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,3 +1,9 @@
-const { toMatchImageSnapshot } = require("jest-image-snapshot");
+const { configureToMatchImageSnapshot } = require("jest-image-snapshot");
+
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+  // Permit 0.5% difference.
+  failureThreshold: "0.005",
+  failureThresholdType: "percent"
+});
 
 expect.extend({ toMatchImageSnapshot });

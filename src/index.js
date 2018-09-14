6b641bb94a8228fdbb7e1645aeb5e2a01a957b80
Thilo Maier <git@maier.asia>
2018-09-14 11:16:01 -0400

    Add NewTabLink and PostHeader to exports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { Heading, Link, Text } from "rebass";
 import Footer from "./composites/Footer";
 import Header from "./composites/Header";
 import HeadingPage from "./pages/HeadingPage";
+import NewTabLink from "./primitives/NewTabLink";
+import PostHeader from "./primitives/PostHeader";
 import PostList from "./composites/PostList";
 import Provider from "./helpers/Provider";
 import TextPage from "./pages/TextPage";
@@ -12,6 +14,8 @@ export {
   Heading,
   HeadingPage,
   Link,
+  NewTabLink,
+  PostHeader,
   PostList,
   Provider,
   Text,

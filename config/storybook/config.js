a3a37933362722fa6905355fc5a0ad8d689c9f8a
Thilo Maier <git@maier.asia>
2018-09-19 11:45:32 -0400

    Add Knobs plugin to Storybook

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -2,6 +2,7 @@ import React from "react";
 import { configure, addDecorator, setAddon } from "@storybook/react";
 import { setOptions } from "@storybook/addon-options";
 import JSXAddon from "storybook-addon-jsx";
+import { withKnobs } from "@storybook/addon-knobs/react";
 import Provider from "../../src/helpers/Provider";
 import pkg from "../../package.json";
 
@@ -11,13 +12,14 @@ setOptions({
   addonPanelInRight: true
 });
 
-setAddon(JSXAddon);
-
 // Add decorators before require.context:
 // https://github.com/storybooks/storybook/issues/3246
 
 // eslint-disable-next-line
 addDecorator(story => <Provider>{story()}</Provider>);
+addDecorator(withKnobs);
+
+setAddon(JSXAddon);
 
 function loadStories() {
   // Sort order of nested stories is determined by loading order.
--- src/primitives/Icon/Icon.stories.jsx
+++ src/primitives/Icon/Icon.stories.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { select, text } from "@storybook/addon-knobs/react";
 import Icon from "./Icon";
 import NewTabLink from "../NewTabLink";
 
 storiesOf("Primitives/Icon", module)
-  .addWithJSX("GitHub", () => <Icon type="github" username="mdotasia" m={2} />)
-  .addWithJSX("Twitter", () => (
-    <Icon type="twitter" username="mdotasia" m={2} />
+  .addWithJSX("knobs", () => (
+    <Icon
+      type={select("type", { github: "GitHub", twitter: "Twitter" }, "github")}
+      username={text("username", "mdotasia")}
+      color={text("color", "accent")}
+      m={2}
+    />
   ))
-  .addWithJSX("custom anchor", () => (
+  .addWithJSX("custom link", () => (
     <Icon link={NewTabLink} type="github" username="mdotasia" m={2} />
-  ))
-  .addWithJSX("custom color", () => (
-    <Icon color="alternate" type="github" username="mdotasia" m={2} />
   ));

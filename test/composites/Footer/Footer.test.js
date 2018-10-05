6a41c74e886d08ce2e785ae811468837735c1b3d
Thilo Maier <git@maier.asia>
2018-10-04 21:29:30 -0400

    Download snapshots from server

--- test/composites/Footer/Footer.test.js
+++ test/composites/Footer/Footer.test.js
@@ -1,16 +1,26 @@
 import * as deviceDescriptors from "puppeteer/DeviceDescriptors";
 import devices from "../../../src/devices";
+import {
+  createCustomSnapshotIdentifier,
+  downloadScreenshot
+} from "../../helpers";
 
 const file = `file://${process.cwd()}/storybook-static/index.html?selectedKind=Composites%2FFooter&selectedStory=last%20updated&full=1`;
 
 describe("Footer", () => {
   devices.forEach(device => {
-    test(device, async () => {
+    const customSnapshotIdentifier = createCustomSnapshotIdentifier(device);
+    test(customSnapshotIdentifier, async () => {
+      await downloadScreenshot({
+        componentName: "Footer",
+        customSnapshotIdentifier,
+        cwd: __dirname
+      });
       await page.emulate(deviceDescriptors[device]);
       await page.goto(file);
       const image = await page.screenshot();
       expect(image).toMatchImageSnapshot({
-        customSnapshotIdentifier: device.toLocaleLowerCase().replace(/ /g, "-")
+        customSnapshotIdentifier
       });
     });
   });

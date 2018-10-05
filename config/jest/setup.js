9bcd01b8ceaf207b5d432bf6cdf8cd60b537f56f
Thilo Maier <git@maier.asia>
2018-10-05 16:41:58 -0400

    Image reporter distinguishes between screenshots and diffs

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,7 +1,7 @@
 const { configureToMatchImageSnapshot } = require("jest-image-snapshot");
 
-// Increase from 5000ms to 10000ms to give enough time for screenshot downloads.
-jest.setTimeout(10000);
+// Increase from 5000ms to 15000ms to give enough time for screenshot downloads.
+jest.setTimeout(15000);
 
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
   // Permit 0.5% difference.

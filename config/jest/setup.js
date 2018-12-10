2739429f178c32ec8a5846f32a3c2ddae0888b6c
Thilo Maier <git@maier.asia>
2018-12-10 12:43:49 +0100

    Increase Jest timeout to accomodate large screenshots

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,7 +1,8 @@
 const { configureToMatchImageSnapshot } = require("jest-image-snapshot");
 
-// Increase from 5000ms to 15000ms to give enough time for screenshot downloads.
-jest.setTimeout(15000);
+// Increase from 5000ms to 25000ms to give enough time test to complete.
+// Downloading large screenshots and making diffs of big screenshot files takes some time.
+jest.setTimeout(25000);
 
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
   // Permit 0.1% difference.

dee6c00fb6e926fd72df068bace24de543c5fb0d
Thilo Maier <git@maier.asia>
2019-04-29 18:42:53 +0000

    Remove page templates (closes #15)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import { Box, Flex, Heading, Link, Text } from 'rebass';
 import Footer from './composites/Footer';
 import Head from './helpers/Head';
 import Header from './composites/Header';
-import HeadingPage from './pages/HeadingPage';
 import NewTabLink from './primitives/NewTabLink';
 import PostHeader from './primitives/PostHeader';
 import PostList from './composites/PostList';
 import Provider from './helpers/Provider';
-import TextPage from './pages/TextPage';
 
 export {
   Box,
@@ -16,12 +14,10 @@ export {
   Head,
   Header,
   Heading,
-  HeadingPage,
   Link,
   NewTabLink,
   PostHeader,
   PostList,
   Provider,
   Text,
-  TextPage,
 };

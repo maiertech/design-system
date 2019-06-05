5d93653b4ba4690a882dcf1015a6444f893c0f0d
Thilo Maier <git@maier.asia>
2019-06-04 21:05:02 -0400

    Increase timeout for visual regression tests

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,8 +1,8 @@
 const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
 
-// Increase from 5000ms to 25000ms to give enough time test to complete.
+// Increase from 60000ms to give tests enough time to complete.
 // Downloading large screenshots and making diffs of big screenshot files takes some time.
-jest.setTimeout(25000);
+jest.setTimeout(60000);
 
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
   // Permit 0.1% difference.

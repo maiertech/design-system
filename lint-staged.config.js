d22bb466332ab839de6f4c66e22ef4cbdb51fcb9
Thilo Maier <git@maier.asia>
2021-01-09 03:27:27 +0000

    Upgrade lint-staged and config Fixes #198

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,22 @@
-module.exports = {
-  '*.js': ['eslint --fix', 'prettier --write', 'prettier --check'],
-  '*.{md,mdx,css,yaml,yml}': ['prettier --write', 'prettier --check'],
+const micromatch = require('micromatch');
+const prettier = require('prettier');
+
+// Figure out all extensions supported by Prettier.
+const prettierSupportedExtensions = prettier
+  .getSupportInfo()
+  .languages.map(({ extensions }) => extensions)
+  .flat();
+
+module.exports = (allStagedFiles) => {
+  // Match files for ESLint.
+  const eslintFiles = micromatch(allStagedFiles, ['**/*.js']);
+  // Match all files that can be formated by Prettier.
+  const prettierFiles = micromatch(
+    allStagedFiles,
+    prettierSupportedExtensions.map((extension) => `**/*${extension}`)
+  );
+  return [
+    `eslint --fix ${eslintFiles.join(' ')}`,
+    `prettier --write ${prettierFiles.join(' ')}`,
+  ];
 };
